17897412f8c201c26c7723558f255012e61fb03d
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-05-06 02:47:19 +0300

    Add prettier rc

--- utils/colorSystem/colorSystem.ts
+++ utils/colorSystem/colorSystem.ts
@@ -1,15 +1,8 @@
-import { isUndefined } from "lodash";
+import { isUndefined } from 'lodash'
 
-import { isServerSide } from "../helpers";
-import { ColorTheme, StyleProps, ThemePreferences } from "./interfaces";
-import { getThemeCookies } from "./cookieFetcher";
-
-// TODO: define descriptions for all functions which might be used externally
-
-const validateCache = () => {
-  // TODO: check if realm based themes are saved in public dir, if not -> fetch them
-  // ? Should be run on log in
-};
+import { isServerSide } from '../helpers'
+import { ColorTheme, StyleProps, ThemePreferences } from './interfaces'
+import { getThemeCookies } from './cookieFetcher'
 
 /**
  * Load theme preferences for the realm and the user
@@ -18,38 +11,31 @@ const validateCache = () => {
  * @return {ThemePreferences} theme preferences
  */
 
-const loadTheme = (
-  themeName: string | undefined,
-  themeMode: string | undefined
-): ColorTheme => {
-  // try to load preffered theme
-  // if ^ fails, try to load default theme
-  try {
-    return require(`../../public/themes/${themeName}.${themeMode}.json`) as ColorTheme;
-  } catch (error) {
-    console.error(error);
+const loadTheme = (themeName: string | undefined, themeMode: string | undefined): ColorTheme => {
+    // try to load preffered theme
+    // if ^ fails, try to load default theme
+    try {
+        return require(`../../public/themes/${themeName}.${themeMode}.json`) as ColorTheme
+    } catch (error) {
+        console.error(error)
 
-    // default to light mode if user preferences weren't fetched
-    return require(`../../public/themes/default.${
-      isUndefined(themeMode) ? "light" : themeMode
-    }.json`) as ColorTheme;
-  }
-};
+        // default to light mode if user preferences weren't fetched
+        return require(`../../public/themes/default.${isUndefined(themeMode) ? 'light' : themeMode}.json`) as ColorTheme
+    }
+}
 
 /**
  * This function retrieves theme-dependent styles for a specified React component.
  * @param {string} componentName - The name of the React component for which to fetch styling.
  * @return {Object} A JavaScript object containing CSS styling.
  */
-export const getStyledComponentObject = (
-  componentName: string
-): StyleProps | undefined => {
-  // do not run on client
-  if (!isServerSide()) return;
+export const getStyledComponentObject = (componentName: string): StyleProps | undefined => {
+    // do not run on client
+    if (!isServerSide()) return
 
-  const { themeName, themeMode } = getThemeCookies();
+    const { themeName, themeMode } = getThemeCookies()
 
-  const theme = loadTheme(themeName, themeMode);
+    const theme = loadTheme(themeName, themeMode)
 
-  return theme.components[componentName];
-};
+    return theme.components[componentName]
+}
