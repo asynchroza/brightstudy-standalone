61e9ac0440a38eb4eccfdc61ce161f98966c2316
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-05-06 02:48:10 +0300

    Add todo in cookieFetcher

--- utils/colorSystem/cookieFetcher.ts
+++ utils/colorSystem/cookieFetcher.ts
@@ -1,9 +1,11 @@
-import { cookies } from "next/headers";
+import { cookies } from 'next/headers'
+
+//TODO: Load preferences if cookies are empty, setCookie as well
 
 export const getThemeCookies = () => {
-  const cookieStore = cookies();
-  const themeName = cookieStore.get("themeName")?.value;
-  const themeMode = cookieStore.get("themeMode")?.value;
+    const cookieStore = cookies()
+    const themeName = cookieStore.get('themeName')?.value
+    const themeMode = cookieStore.get('themeMode')?.value
 
-  return { themeName, themeMode };
-};
+    return { themeName, themeMode }
+}
