dc4029cef3b27e246f58de6034ddb6c95ee5cba6
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-05-20 20:40:39 +0300

    Add graphql

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,50 @@
-console.log("HELLO WORLD");
+import { ApolloServer } from '@apollo/server';
+import { PrismaClient } from '@prisma/client';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { DateTimeResolver } from 'graphql-scalars';
+
+const prisma = new PrismaClient();
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+  scalar DateTime
+
+  type User {
+    id: Int!
+    name: String!
+    createdAt: DateTime!
+    messages: [Message!]!
+  }
+
+  type Message {
+    id: Int!
+    body: String!
+    createdAt: DateTime!
+    userId: Int!
+    user: User!
+  }
+
+  type Query {
+    users: [User!]!
+  }
+`;
+
+const resolvers = {
+	DateTime: DateTimeResolver,
+	Query: {
+		users: () => {
+			return prisma.user.findMany();
+		}
+	}
+};
+
+const server = new ApolloServer({ resolvers, typeDefs });
+startStandaloneServer(server, { listen: { port: 4000 } })
+	.then(({ url }) => {
+		console.log(`Server running at ${url}`);
+	})
+	.catch((error) => {
+		console.error('Error starting server:', error);
+	});
