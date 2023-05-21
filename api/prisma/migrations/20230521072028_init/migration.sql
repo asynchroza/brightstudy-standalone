a33e601bc6fbf3c6aefa75c2acf865f100c209cb
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-05-21 10:23:37 +0300

    Add new migration

---
+++ api/prisma/migrations/20230521072028_init/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the `Message` table. If the table is not empty, all the data it contains will be lost.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `firstName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `lastName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "Permission" AS ENUM ('ADMIN', 'PROFESSOR', 'STUDENT');
+
+-- DropForeignKey
+ALTER TABLE "Message" DROP CONSTRAINT "Message_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "createdAt",
+DROP COLUMN "name",
+ADD COLUMN     "email" TEXT NOT NULL,
+ADD COLUMN     "firstName" TEXT NOT NULL,
+ADD COLUMN     "lastName" TEXT NOT NULL,
+ADD COLUMN     "password" TEXT NOT NULL,
+ADD COLUMN     "permissions" "Permission"[];
+
+-- DropTable
+DROP TABLE "Message";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -1,60 +1,33 @@
 // prisma/seed.ts
 
-import { PrismaClient } from "@prisma/client";
+import { Permission, PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  // Delete all `User` and `Message` records
-  await prisma.message.deleteMany({});
-  await prisma.user.deleteMany({});
-  // (Re-)Create dummy `User` and `Message` records
-  await prisma.user.create({
-    data: {
-      name: "Jack",
-      messages: {
-        create: [
-          {
-            body: "A Note for Jack",
-          },
-          {
-            body: "Another note for Jack",
-          },
-        ],
-      },
-    },
-  });
-  await prisma.user.create({
-    data: {
-      name: "Ryan",
-      messages: {
-        create: [
-          {
-            body: "A Note for Ryan",
-          },
-          {
-            body: "Another note for Ryan",
-          },
-        ],
-      },
-    },
-  });
-  await prisma.user.create({
-    data: {
-      name: "Adam",
-      messages: {
-        create: [
-          {
-            body: "A Note for Adam",
-          },
-          {
-            body: "Another note for Adam",
-          },
-        ],
-      },
-    },
-  });
+	// Delete all `User` and `Message` records
+	await prisma.user.deleteMany({});
+	// (Re-)Create dummy `User` and `Message` records
+	await prisma.user.create({
+		data: {
+			firstName: 'Toni',
+			lastName: 'Montana',
+			password: 'password',
+			email: 'tonimontana@gmail.com',
+			permissions: Permission.STUDENT
+		}
+	});
+
+	await prisma.user.create({
+		data: {
+			firstName: 'Kay',
+			lastName: 'Be',
+			password: 'password',
+			email: 'kaybe@gmail.com',
+			permissions: Permission.ADMIN
+		}
+	});
 }
 
 main().then(() => {
-  console.log("Data seeded...");
+	console.log('Data seeded...');
 });
