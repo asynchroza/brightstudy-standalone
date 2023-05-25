da828bc8a3eabff15daf9281733fec12c59e1829
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-05-25 23:28:07 +0300

    Update error wrapper comment

--- api/utils/exceptions.ts
+++ api/utils/exceptions.ts
@@ -1,5 +1,12 @@
 import logger from './logger';
 
+/**
+ * Wrapper that throws an exception and logs its message through pino
+ * @summary This wrapper is intended for important exceptions that have significant impact on the end customer.
+ * An example would be if we deployed the portal without setting up the JWT_SECRET. In such cases, a logged exception is crucial.
+ * However, for exceptions that occur during regular occurrences like authentication failures, they are not of great importance.
+ * @param {string | Error} error - The error message or error object to be thrown and logged
+ */
 const throwLoggedException = (error: string | Error) => {
 	if (typeof jest !== undefined) {
 		return;
